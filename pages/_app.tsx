bb7b4564c0996c8100ec4420ec7a6efe37977020
devavi-labs <devavilabs@gmail.com>
2021-06-13 21:35:54 +0530

    add: prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import { NextPage } from 'next';
-import { AppProps } from 'next/dist/next-server/lib/router/router';
-import '../styles/globals.css';
+import { NextPage } from "next"
+import { AppProps } from "next/dist/next-server/lib/router/router"
+import "../styles/globals.css"
 
-const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />
 
-export default MyApp;
+export default MyApp
