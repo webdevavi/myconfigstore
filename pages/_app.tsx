90d413081da8e70a5913d5035653a630b14e8167
devavi-labs <devavilabs@gmail.com>
2021-06-13 21:51:04 +0530

    add: chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next"
 import { AppProps } from "next/dist/next-server/lib/router/router"
-import "../styles/globals.css"
+import { ChakraProvider } from "@chakra-ui/react"
+import theme from "../theme"
 
-const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
+	<ChakraProvider theme={theme}>
+		<Component {...pageProps} />
+	</ChakraProvider>
+)
 
 export default MyApp
