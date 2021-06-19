bd9aff24e43de0c51e6b66b4b7b419acd2c1d888
devavi-labs <devavilabs@gmail.com>
2021-06-20 02:45:03 +0530

    add: skeleton policy pages

---
+++ pages/[policy].tsx
@@ -0,0 +1,65 @@
+import { Container } from "@chakra-ui/react"
+import { DefaultNavbar, Markdown } from "@components"
+import { GetStaticPaths, GetStaticProps, NextPage } from "next"
+import Head from "next/head"
+import { ParsedUrlQuery } from "querystring"
+import React from "react"
+
+interface TermsPageProps {
+	markdown: string
+	title: string
+}
+
+const TermsPage: NextPage<TermsPageProps> = ({ markdown, title }) => {
+	return (
+		<div>
+			<Head>
+				<title>{title} | myconfig.store</title>
+				<meta name="description" content="A simple, fast, secure and highly available remote store for all your dynamic configs." />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<DefaultNavbar />
+			<Container maxW="960px">
+				<Markdown>{markdown}</Markdown>
+			</Container>
+		</div>
+	)
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const arr: string[] = ["terms", "privacy", "refund", "cancellation"]
+	const paths = arr.map((policy) => {
+		return {
+			params: { policy },
+		}
+	})
+	return { paths, fallback: false }
+}
+
+interface IParams extends ParsedUrlQuery {
+	policy: string
+}
+
+export const getStaticProps: GetStaticProps<TermsPageProps, IParams> = async ({ params }) => {
+	const { policy } = params!
+
+	let title = "Terms & Conditions"
+
+	if (policy === "privacy") {
+		title = "Privacy Policy"
+	}
+
+	if (policy === "refund") {
+		title = "Refund Policy"
+	}
+
+	if (policy === "cancellation") {
+		title = "Cancellation Policy"
+	}
+
+	const { default: markdown } = await require(`../policies/${String(policy).toUpperCase()}.md`)
+
+	return { props: { markdown, title } }
+}
+
+export default TermsPage
---
+++ policies/REFUND.md
@@ -0,0 +1,1 @@
+# Refund Policy `TODO`
