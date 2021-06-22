9c83c82afeabb7eb4992336c3dba8f6b00755a4f
devavi-labs <devavilabs@gmail.com>
2021-06-23 00:29:02 +0530

    add: logo, favicon

--- components/Logo/text/index.tsx
+++ components/Logo/text/index.tsx
@@ -1,10 +1,29 @@
-import { Heading, HeadingProps } from "@chakra-ui/react"
+import { Heading, HeadingProps, HStack, Img } from "@chakra-ui/react"
 import React from "react"
 
-export const TextLogo: React.FC<HeadingProps> = (props) => {
+export const TextLogo: React.FC<HeadingProps> = ({ fontSize = "xl", ...props }) => {
+	const imgWidth = {
+		xs: 4,
+		sm: 6,
+		md: 8,
+		lg: 10,
+		xl: 12,
+		"2xl": 14,
+		"3xl": 16,
+		"4xl": 18,
+		"5xl": 20,
+		"6xl": 22,
+		"7xl": 24,
+		"8xl": 26,
+		"9xl": 28,
+	}
+
 	return (
-		<Heading as="span" color="brand.orange" fontSize="xl" {...props}>
-			myconfig.store
-		</Heading>
+		<HStack>
+			<Img src="/myconfigstore-logo-196x196.png" alt="MyConfigStore Logo" width={imgWidth[fontSize as keyof typeof imgWidth] ?? 12} />
+			<Heading as="span" color="brand.orange" fontSize={fontSize} {...props}>
+				myconfig.store
+			</Heading>
+		</HStack>
 	)
 }
