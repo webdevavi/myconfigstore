5387fadca2a66c65ee5229258875744ac629a745
devavi-labs <devavilabs@gmail.com>
2021-06-26 16:21:58 +0530

    add: address in contact us page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,9 @@
-import { Box, Container, Icon, Heading, StackProps, Link, VStack } from "@chakra-ui/react"
+import { Box, Container, Icon, Heading, StackProps, Link, Text, VStack } from "@chakra-ui/react"
 import { DefaultNavbar, AnimatedLogo, Footer } from "@components"
 import { NextPageWithSEO } from "@lib/types"
 import { motion, MotionProps } from "framer-motion"
 import React from "react"
-import { FaEnvelope, FaTwitter, FaFacebook, FaPhoneAlt } from "react-icons/fa"
+import { FaEnvelope, FaTwitter, FaFacebook, FaPhoneAlt, FaHome } from "react-icons/fa"
 
 const MotionVStack = motion<Omit<StackProps, "transition"> & MotionProps>(VStack as any)
 
@@ -27,22 +27,42 @@ const ContactPage: NextPageWithSEO = () => {
 					<Box d="flex" flexWrap="wrap" justifyContent="center" spacing="8">
 						<VStack flex="1" m="4">
 							<Icon as={FaTwitter} fontSize="3xl" color="twitter.500" />
-							<Link href="https://twitter.com/myconfigstore">@myconfigstore</Link>
+							<Link color="brand.light" href="https://twitter.com/myconfigstore">
+								@myconfigstore
+							</Link>
 						</VStack>
 
 						<VStack flex="1" m="4">
 							<Icon as={FaFacebook} fontSize="3xl" color="facebook.400" />
-							<Link href="https://facebook.com/myconfigstore">@myconfigstore</Link>
+							<Link color="brand.light" href="https://facebook.com/myconfigstore">
+								@myconfigstore
+							</Link>
 						</VStack>
 
 						<VStack flex="1" m="4">
 							<Icon as={FaPhoneAlt} fontSize="3xl" color="telegram.500" />
-							<Link href="tel:+918580309667">+918580309667</Link>
+							<Link color="brand.light" href="tel:+918580309667">
+								+918580309667
+							</Link>
 						</VStack>
 					</Box>
 					<VStack flex="1" m="2">
 						<Icon as={FaEnvelope} fontSize="3xl" color="red.400" />
-						<Link href="mailto:myconfigstore@gmail.com">myconfigstore@gmail.com</Link>
+						<Link color="brand.light" href="mailto:myconfigstore@gmail.com">
+							myconfigstore@gmail.com
+						</Link>
+					</VStack>
+					<VStack flex="1" m="2">
+						<Icon as={FaHome} fontSize="3xl" color="brand.orange" />
+						<Text as="p" textAlign="center" opacity="0.8" whiteSpace="pre-line">
+							<strong>Mr. Avinash Sinha</strong>
+							<br />
+							Road No. 18, Quarter No. E/268
+							<br />
+							P.T.P.S. Patratu, Ramgarh - 829119
+							<br />
+							Jharkhand - India 🇮🇳
+						</Text>
 					</VStack>
 				</MotionVStack>
 			</Container>
