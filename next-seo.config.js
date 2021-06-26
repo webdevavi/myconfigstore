63b0443156ad2f3e28893d1c01850b7a0f1b53f1
devavi-labs <devavilabs@gmail.com>
2021-06-26 18:39:18 +0530

    refactor: changed title in seo to myconfig only

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,7 +1,7 @@
 /** @type {import("next-seo").NextSeoProps} */
 const seo = {
-	defaultTitle: "MyConfigStore",
-	titleTemplate: "%s | MyConfigStore",
+	defaultTitle: "MyConfig",
+	titleTemplate: "%s | MyConfig",
 	canonical: process.env.NEXT_PUBLIC_CANONICAL_URL,
 	description: "A simple, fast, secure and highly available remote store for all your dynamic configs.",
 	twitter: {
@@ -13,13 +13,13 @@ const seo = {
 		appId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID ?? "",
 	},
 	openGraph: {
-		site_name: "MyConfigStore",
+		site_name: "MyConfig",
 		url: process.env.NEXT_PUBLIC_CANONICAL_URL,
 		type: "website",
 		images: [
 			{
 				url: `${process.env.NEXT_PUBLIC_CANONICAL_URL}/banner.jpg`,
-				alt: "MyConfigStore Banner",
+				alt: "MyConfig Banner",
 				height: 876.84,
 				width: 1666,
 			},
